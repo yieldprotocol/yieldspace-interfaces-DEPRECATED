fb7e40144f7bc5868d515891eff63ab72a957c30
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-04-06 11:27:39 +0100

    2.0.8: PoolRouter

---
+++ IPoolRouter.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.1;
+
+import "@yield-protocol/yieldspace-interfaces/IPool.sol";
+import "./PoolDataTypes.sol";
+
+
+interface IPoolRouter {
+  
+  function batch(
+    address[] calldata bases,
+    address[] calldata fyTokens,
+    uint8[] calldata targets,
+    PoolDataTypes.Operation[] calldata operations,
+    bytes[] calldata data
+  ) external payable;
+  
+  function route(address base, address fyToken, bytes calldata data)
+    external payable returns (bool success, bytes memory result);
+}
