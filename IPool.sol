ad3d75fcbc2a59d80f778bd8b26646268fd8cfbc
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-04-09 13:18:44 +0100

    2.0.10: mint calculateFromBase

--- IPool.sol
+++ IPool.sol
@@ -21,7 +21,7 @@ interface IPool is IERC20, IERC2612 {
     function buyBaseTokenPreview(uint128 baseTokenOut) external view returns(uint128);
     function sellFYTokenPreview(uint128 fyTokenIn) external view returns(uint128);
     function buyFYTokenPreview(uint128 fyTokenOut) external view returns(uint128);
-    function mint(address to, uint256 minTokensMinted) external returns (uint256, uint256, uint256);
+    function mint(address to, bool calculateFromBase, uint256 minTokensMinted) external returns (uint256, uint256, uint256);
     function mintWithBaseToken(address to, uint256 fyTokenToBuy, uint256 minTokensMinted) external returns (uint256, uint256, uint256);
     function burn(address to, uint256 minBaseTokenOut, uint256 minFYTokenOut) external returns (uint256, uint256, uint256);
     function burnForBaseToken(address to, uint256 minBaseTokenOut, uint256 minFYTokenOut) external returns (uint256, uint256, uint256);
