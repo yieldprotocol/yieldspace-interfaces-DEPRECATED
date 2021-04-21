8a1b77ea3b5ae0aa399a6dc1d3222848b7f14936
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-04-21 16:04:42 +0100

    2.0.11: Use yield-utils-v2

--- IPool.sol
+++ IPool.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity >= 0.8.0;
-import "@yield-protocol/utils/contracts/token/IERC20.sol";
-import "@yield-protocol/utils/contracts/token/IERC2612.sol";
+import "@yield-protocol/utils-v2/contracts/token/IERC20.sol";
+import "@yield-protocol/utils-v2/contracts/token/IERC2612.sol";
 import "@yield-protocol/vault-interfaces/IFYToken.sol";
 
 
