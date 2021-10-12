a6fb34026407a58b4b81a9596c12934095fb32b9
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-10-12 07:09:56 +0100

    2.3.0-rc5: constants, ratio, invariant

--- IPool.sol
+++ IPool.sol
@@ -6,10 +6,14 @@ import "@yield-protocol/vault-interfaces/IFYToken.sol";
 
 
 interface IPool is IERC20, IERC2612 {
+    function ts() external view returns(int128);
+    function g1() external view returns(int128);
+    function g2() external view returns(int128);
+    function maturity() external view returns(uint32);
+    function scaleFactor() external view returns(uint96);
     function getCache() external view returns (uint112, uint112, uint32);
     function base() external view returns(IERC20);
     function fyToken() external view returns(IFYToken);
-    function maturity() external view returns(uint32);
     function getBaseBalance() external view returns(uint112);
     function getFYTokenBalance() external view returns(uint112);
     function retrieveBase(address to) external returns(uint128 retrieved);
@@ -22,9 +26,8 @@ interface IPool is IERC20, IERC2612 {
     function buyBasePreview(uint128 baseOut) external view returns(uint128);
     function sellFYTokenPreview(uint128 fyTokenIn) external view returns(uint128);
     function buyFYTokenPreview(uint128 fyTokenOut) external view returns(uint128);
-    function mint(address to, bool calculateFromBase, uint256 minTokensMinted) external returns (uint256, uint256, uint256);
-    function mintWithBase(address to, uint256 fyTokenToBuy, uint256 minTokensMinted) external returns (uint256, uint256, uint256);
-    function burn(address baseTo, address fyTokenTo, uint256 minBaseOut, uint256 minFYTokenOut) external returns (uint256, uint256, uint256);
-    function burnForBase(address to, uint256 minBaseOut) external returns (uint256, uint256);
-    function invariant() external returns (uint128);
+    function mint(address to, bool calculateFromBase, uint256 minRatio) external returns (uint256, uint256, uint256);
+    function mintWithBase(address to, uint256 fyTokenToBuy, uint256 minRatio) external returns (uint256, uint256, uint256);
+    function burn(address baseTo, address fyTokenTo, uint256 minRatio) external returns (uint256, uint256, uint256);
+    function burnForBase(address to, uint256 minRatio) external returns (uint256, uint256);
 }
